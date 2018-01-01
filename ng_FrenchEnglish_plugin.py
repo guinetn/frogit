bf6ba42e48f274a23509b25e13b496a26cb71a05
Guinet <nguinet.pro@gmail.com>
2018-01-01 23:13:34 +0100

    First Release

---
+++ ng_FrenchEnglish_plugin.py
@@ -0,0 +1,25 @@
+import sublime
+import sublime_plugin
+import re
+
+class FenCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sels = self.view.sel()
+		found = False
+		for sel in sels:
+			term = self.view.substr(sel)
+			if (len(sel) < 3):
+				return
+			# debug self.view.insert(edit, 0, "Searching..." +  term)
+
+			pattern = re.compile("	(" + term + ")[ \t]+(.*)")
+
+			for i, line in enumerate(open(r'N:\@md\md\.ENGLISH_FRENCH.md')):
+				for match in re.finditer(pattern, line):
+				 # debug  res = 'Found on line %s: %s' % (i+1, match.group(2))
+				 res = "<<" + match.group(2) + ">> "
+				 self.view.insert(edit, 0, res)
+				found = True
+		if (not found):
+			self.view.insert(edit, 0, "<<NOT FOUND>>")	
+

a7a167bd6aa263940e668b02c389a61f52e525dc
Guinet <nguinet.pro@gmail.com>
2018-01-03 22:58:31 +0100

    Accept multi cursors translation + beautify popup

--- frogit.py
+++ frogit.py
@@ -16,21 +16,23 @@ class FrogitCommand(sublime_plugin.TextCommand):
 
 				for line in open(enfr):
 					for match in re.finditer(pattern, line):
-						res = match.group(2) 						
+						res += "• " + term + ": " + match.group(2) + '<br>'
 						found = True
 
 				html = """
 			    <body>
 			        <style>
-			            div { color: yellow; border:solid 1px black; }			         
+			        	.arrow-down { width: 0; height: 0; border-style: solid; border-width: 7px 5px; border-color: #ffd500 transparent transparent transparent; }
+			            div { color: #ffd500; border:solid 1px black; margin-top:-5px; }			         
 			        </style>
+			        <div class="arrow-down"></div>
 			        <div>%s</div>			        
 			    </body>
 				""" % (res)	
-				self.view.show_popup(html, max_width=512, on_navigate=lambda x:copy(self.view, x))	
+				self.view.show_popup(html, max_width=256)	
 								
 				# Add missing translation in the en/fr file. Todo: automatic add in right position with a simple translation
 				if (not found):
 					with open(enfr, "a") as f:						
-						f.write(term)
+						f.write("\r\n"+term)
 		    			

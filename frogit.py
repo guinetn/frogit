7bd07583ba9744bcca09038f98fd4a4664892ec0
Guinet <nguinet.pro@gmail.com>
2018-01-15 19:50:18 +0100

    Update frogit.py
    Add [case, plural] insensitive search

--- frogit.py
+++ frogit.py
@@ -3,16 +3,21 @@ import sublime_plugin
 import re
 
 # Dictionnary file path
-dictionnary = r'C:\My\ENGLISH_FRENCH.md'
+dictionnary = r'N:\@md\md\.ENGLISH_FRENCH.md'
 
 class FrogitCommand(sublime_plugin.TextCommand):
 	def run(self, edit):				
 		translated = ""
 		term = ""
-		for region in self.view.sel():  #get user selection
+		for region in self.view.sel():  #get user selections
 			term = self.view.substr(self.view.word(region))
+			print(term)
 			if len(term) > 2:
-				pattern = re.compile("	(" + term + ")[ \t]+(.*)")
+				# Remove trailing 's'
+				if term[-1].lower() == 's':
+					term = term[0:-1]
+					print(term)
+				pattern = re.compile("	(" + term + ")[ \t]+(.*)", re.I)  # Case insensitive
 				for line in open(dictionnary):
 					for match in re.finditer(pattern, line):
 						translated += "• " + term + ": " + match.group(2) + '<br>'
@@ -28,7 +33,7 @@ class FrogitCommand(sublime_plugin.TextCommand):
 				""" % (translated)
 				self.view.show_popup(html, max_width=256)
 
-				# Add missing translation in the dictionnay. Todo: Use an api to automatic add the translation in right position
-				if (len(translated)=0):
-					with open(dictionnary, "a") as f:
-						f.write("\r\n"+term)
+		# Add missing translation in the dictionnay. Todo: Use an api to automatic add the translation in right position
+		if (len(translated) == 0):
+			with open(dictionnary, "a") as f:
+				f.write("\r\n"+term)		    			
